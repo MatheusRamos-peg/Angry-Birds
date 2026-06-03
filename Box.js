ff9c7c3ca7694db64e9a1a4c82e6c221b407a826
Matheus <matferrao2013@gmail.com>
2026-06-03 19:22:45 -0400

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,17 +1,26 @@
-class Box{
-    constructor(x,y,width,height, imagem, xScale, yScale){
-        this.body = Bodies.rectangle(x, y, width, height, {
-            restitution: 0.2,
-            render:{
-                sprite:{
-                    texture:imagem,
-                    xScale: xScale,
-                    yScale: yScale,
-                }
-            }
-        });
-    }
-    addToWorld(world) {
-        Composite.add(world, this.body);
-    }
+class Box {
+
+  constructor(x,y,width,height,imagem){
+
+    this.body = Bodies.rectangle(x, y, width, height, {
+
+      restitution: 0.2,
+
+      render:{
+        sprite:{
+          texture: imagem,
+
+          xScale: 1,
+          yScale: 1
+        }
+      }
+
+    });
+
+  }
+
+  addToWorld(world) {
+    Composite.add(world, this.body);
+  }
+
 }
