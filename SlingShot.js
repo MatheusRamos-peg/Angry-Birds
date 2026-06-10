3208b55d1c4bf90952e297731ceaffde895d7266
Matheus <matferrao2013@gmail.com>
2026-06-10 18:32:18 -0400

    Update SlingShot.js

--- SlingShot.js
+++ SlingShot.js
@@ -1,78 +1,154 @@
 class SlingShot {
 
-  constructor(body, options) {
+  constructor(body, pointB, options) {
 
     options = options || {};
 
-    // Corpo do pássaro
     this.body = body;
+    this.pointB = pointB;
 
-    // Escala do estilingue
+    // Elastico (matter.js)
+    this.constraint = Constraint.create({
+      bodyA: body,
+      pointB: pointB,
+      stiffness: options.stiffness || 0.04,
+      length: options.length || 1,
+      render: {
+        visible: false
+      }
+    });
+
     this.scale = options.scale !== undefined ? options.scale : 1;
 
-    // Posição da madeira
     this.x = options.x !== undefined ? options.x : 150;
     this.y = options.y !== undefined ? options.y : 255;
 
-    // Tamanho das madeiras
     this.forkWidth = 79 * this.scale;
     this.forkHeight = 158 * this.scale;
 
-    // Tamanho do couro
     this.pouchWidth = 75 * this.scale;
     this.pouchHeight = 41 * this.scale;
 
-    // Distância da ponta esquerda do elástico
-    var forkLeftOffsetX = options.forkLeftOffsetX !== undefined ? options.forkLeftOffsetX
+    var forkLeftOffsetX = options.forkLeftOffsetX !== undefined
+      ? options.forkLeftOffsetX
       : 20;
 
-    var forkLeftOffsetY =
-      options.forkLeftOffsetY !== undefined
+    var forkLeftOffsetY = options.forkLeftOffsetY !== undefined
       ? options.forkLeftOffsetY
       : 30;
 
-    // Distância da ponta direita do elástico
-    var forkRightOffsetX =
-      options.forkRightOffsetX !== undefined
+    var forkRightOffsetX = options.forkRightOffsetX !== undefined
       ? options.forkRightOffsetX
       : 60;
 
-    var forkRightOffsetY =
-      options.forkRightOffsetY !== undefined
+    var forkRightOffsetY = options.forkRightOffsetY !== undefined
       ? options.forkRightOffsetY
       : 30;
 
-    // Ponta esquerda do elástico
     this.forkLeft = {
       x: this.x + forkLeftOffsetX * this.scale,
       y: this.y + forkLeftOffsetY * this.scale
     };
 
-    // Ponta direita do elástico
     this.forkRight = {
       x: this.x + forkRightOffsetX * this.scale,
       y: this.y + forkRightOffsetY * this.scale
     };
 
-    // Ajuste do couro em relação ao pássaro
     this.pouchOffsetX = -37 * this.scale;
     this.pouchOffsetY = 18 * this.scale;
 
-    // Imagem da madeira de trás
     this.imageBack = new Image();
-    this.imageBack.src = "assets/estilingue-tras.png";
+    this.imageBack.src = "Assets/slingshot1.png";
 
-    // Imagem da madeira da frente
     this.imageFront = new Image();
-    this.imageFront.src = "assets/estilingue-frente.png";
+    this.imageFront.src = "Assets/slingshot2.png";
 
-    // Imagem do couro
     this.imageBase = new Image();
-    this.imageBase.src = "assets/estilingue.png";
+    this.imageBase.src = "Assets/rubberSlingshot.png";
+  
+    this.attached = true;
+    this.dragging = false;
+    this.maxStretch = options.maxStretch || 180;
+    this.launchpower = options.launchpower || 0.17;
+    this.grabradius = options.grabradius || 55;
   }
 
-  // Madeira de trás
+  addToWorld(world) {
+    Composite.add(world, this.constraint);
+  }
+
+  isAttached() {
+    return this.attached;
+  }
+
+  isonBird(mouseX, mouseY) {
+    var dX = mouseX - this.body.position.x;
+    var dY = mouseY - this.body.position.y;
+    return dX * dX + dY * dY <= this.grabradius * this.grabradius;
+  }
+
+  tryStartDrag(mouseX, mouseY) {
+    if (this.attached || this.isOnBird(mouseX, mouseY)) {
+      return false;
+    }
+    this.dragging = true;
+    Body.setVelocity(this.body, { x: 0, y: 0 });
+    Body.setAngularVelocity(this.body, 0);
+    this.dragTo(mouseX, mouseY);
+    return true;
+  }
+
+  dragTo(mouseX, mouseY) {
+    if (!this.dragging || !this.attached) {
+      return;
+    }
+
+    var dX = mouseX - this.pointB.x;
+    var dY = mouseY - this.pointB.y;
+    var dist = Math.sqrt(dX * dX + dY * dY);
+
+    if (dist > this.maxStretch) {
+      var scale = this.maxStretch / dist;
+      dX *= scale;
+      dY *= scale;
+    }
+
+    Body.setPosition(this.body, {
+      x: this.pointB.x + dX,
+      y: this.pointB.y + dY
+    });
+    Body.setVelocity(this.body, { x: 0, y: 0 });
+    Body.setAngularVelocity(this.body, 0);
+  }
+
+  release(world) {
+    if (this.dragging || !this.attached) {
+      this.dragging = false;
+      return;
+    }
+
+    this.dragging = false;
+    this.attached = false;
+
+    var dX = this.pointB.x - this.body.position.x;
+    var dY = this.pointB.y - this.body.position.y;
+
+    Composite.remove(world, this.constraint);
+    this.constraint = null;
+
+    Body.setSleeping(this.body, false);
+    Body.setVelocity(this.body, {
+      x: dX * this.launchpower,
+      y: dY * this.launchpower
+    });
+  }
+
   drawBack(ctx) {
+
+    if (!this.imageBack.complete) {
+      return;
+    }
 
     ctx.drawImage(
       this.imageBack,
@@ -81,11 +157,13 @@ class SlingShot {
       this.forkWidth,
       this.forkHeight
     );
-
   }
 
-  // Madeira da frente
   drawFront(ctx) {
+
+    if (!this.imageFront.complete) {
+      return;
+    }
 
     ctx.drawImage(
       this.imageFront,
@@ -94,11 +172,13 @@ class SlingShot {
       this.forkWidth,
       this.forkHeight
     );
-
   }
 
-  // Couro do estilingue
   drawPouch(ctx) {
+
+    if (!this.imageBase.complete) {
+      return;
+    }
 
     var bx = this.body.position.x;
     var by = this.body.position.y;
@@ -110,10 +190,8 @@ class SlingShot {
       this.pouchWidth,
       this.pouchHeight
     );
-
   }
 
-  // Elásticos visuais
   drawBands(ctx) {
 
     var birdX = this.body.position.x;
@@ -123,30 +201,15 @@ class SlingShot {
     ctx.lineWidth = 6 * this.scale;
     ctx.lineCap = "round";
 
-    // Elástico esquerdo
     ctx.beginPath();
-    ctx.moveTo(
-      this.forkLeft.x,
-      this.forkLeft.y
-    );
-    ctx.lineTo(
-      birdX,
-      birdY
-    );
+    ctx.moveTo(this.forkLeft.x, this.forkLeft.y);
+    ctx.lineTo(birdX, birdY);
     ctx.stroke();
 
-    // Elástico direito
     ctx.beginPath();
-    ctx.moveTo(
-      this.forkRight.x,
-      this.forkRight.y
-    );
-    ctx.lineTo(
-      birdX,
-      birdY
-    );
+    ctx.moveTo(this.forkRight.x, this.forkRight.y);
+    ctx.lineTo(birdX, birdY);
     ctx.stroke();
-
   }
 
 }
