866613fba7d5049f3bbf95280d5d4e71c98650d4
Matheus <matferrao2013@gmail.com>
2026-06-15 19:49:58 -0400

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,26 +1,26 @@
 class Box {
 
-  constructor(x, y, width, height, imagem, scale) {
+  constructor(x,y,width,height,imagem){
 
-    scale = scale === undefined ? 1 : scale;
+    this.body = Bodies.rectangle(x, y, width, height, {
 
-    var scaledWidth = width * scale;
-    var scaledHeight = height * scale;
-
-    this.body = Matter.Bodies.rectangle(x, y, scaledWidth, scaledHeight, {
       restitution: 0.2,
-      render: {
-        sprite: {
+
+      render:{
+        sprite:{
           texture: imagem,
+
           xScale: 1,
           yScale: 1
         }
       }
+
     });
+
   }
 
   addToWorld(world) {
-    Matter.Composite.add(world, this.body);
+    Composite.add(world, this.body);
   }
 
 }
